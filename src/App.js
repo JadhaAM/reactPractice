94e95203329b55aec5b6ded038ace5fc29859003
Amol <amol230703@gmail.com>
2023-06-17 08:30:06 +0530

    # React practice set

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Heder from './Heder';
+function App() {
+  return (
+  <>
+  
+       <Heder name='amol' age='20'> </Heder>
+       <Heder name='sachin' age='19'> </Heder>
+       <Heder name='tejas' age='21'> </Heder>
+       <Heder name='aanand' age='20'> </Heder>
+      
+  </>   
+    
+  );
+}
+
+export default App;
---
+++ src/Heder.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Heder (props){
+    return (
+    <>
+        <h1> this is {props.name} {props.age}</h1> 
+        
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem ex voluptatem aperiam nostrum veniam consequatur
+             alias aliquam. Rerum doloremque iste, animi, cumque ipsam aut corporis est, unde veniam optio voluptates.
+        </p>
+    </>
+       
+        
+  )
+}
+
+export default Heder
